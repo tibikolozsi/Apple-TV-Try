4711b683ac8af6f61d4b9ffd3c1318378be6472c
Tibi Kolozsi <tibi.kolozsi@gmail.com>
2014-08-09 15:01:30 +0200

    Initial Commit

---
+++ AppleTvTry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleTvTry
+//
+//  Created by Tibi Kolozsi on 09/08/14.
+//  Copyright (c) 2014 tibikolozsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppleTvTry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppleTvTry
+//
+//  Created by Tibi Kolozsi on 09/08/14.
+//  Copyright (c) 2014 tibikolozsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
